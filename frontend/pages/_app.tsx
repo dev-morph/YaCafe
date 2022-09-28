02028a12818bb536115e7bc0076884d83f94ecad
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-09-28 23:43:51 +0900

    tailwind

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <h1> Hello world!</h1>
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
