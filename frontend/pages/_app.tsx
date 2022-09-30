5feb53c64ad7204c25bcd707b5deffbb89c6b0f6
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-09-30 23:11:11 +0900

    tailwind 설치 안되는 에러 수정 및 tailwind 설치 완료

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div>
       <h1> Hello world!</h1>
       <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <span className="text-2xl font-bold underline">Hello world!</span>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Component {...pageProps} />
     </div>
   );
