2d01440222c9a6b97775dd3869c301c7a04ea0c7
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-10-07 00:32:45 +0900

    sass 사용

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import styles from '../styles/input.module.scss';
 
 const login = () => {
   return (
     <>
-      <form className="flex-col" action="submit">
+      <form className={styles.form} action="submit">
         <div className="text__input__wrapper">
-          <input type="text" id="id" autoComplete="off" />
-          <label htmlFor="id">ID</label>
+          <input
+            className={styles.form__input}
+            type="text"
+            id="id"
+            autoComplete="off"
+          />
+          <label className="form__label" htmlFor="id">
+            ID
+          </label>
         </div>
       </form>
     </>
