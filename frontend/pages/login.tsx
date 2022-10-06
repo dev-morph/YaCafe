4332297f8410aaec24be3007bb58095f91dfa3f1
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-10-06 23:45:42 +0900

    input css 수정중

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -5,31 +5,9 @@ const login = () => {
     <>
       <form className="flex-col" action="submit">
         <div className="text__input__wrapper">
-          <input
-            id="id"
-            className="text__input"
-            autoComplete="off"
-            type="text"
-          />
-          <label className="input__label" htmlFor="id">
-            <span>ID</span>
-          </label>
+          <input type="text" id="id" autoComplete="off" />
+          <label htmlFor="id">ID</label>
         </div>
-        <div className="text__input__wrapper">
-          <input
-            id="pw"
-            className="text__input"
-            autoComplete="off"
-            type="password"
-          />
-          <label className="input__label" htmlFor="pw">
-            <span>패스워드</span>
-          </label>
-        </div>
-        <input className="text__input" type="password" />
-        <div>asdf</div>
-        <div>asdfasdfasdf</div>
-        <button></button>
       </form>
     </>
   );
